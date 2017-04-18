653934cf0da3c6eb5c022ab45636ea84a2582478
Nicolas Azari <nicodinh@azari.fr>
2017-04-18 22:48:59 +0200

    Hello albums

---
+++ index.ios.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React            from 'react';
+import { Text }         from 'react-native';
+import { AppRegistry }  from 'react-native';
+
+import Header from './src/components/header';
+
+const App = () => {
+  console.log('Started app');
+  return (
+    <Text>Coucou</Text>
+  );
+};
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import React from 'react';
+import { Text } from 'react-native';
+
+const Header = () => {
+  return <Text>Coucou</Text>;
+};
+
+export default Header;
