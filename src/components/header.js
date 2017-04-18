7b9d57a6cab5a786d531b40a4ebe8677401c9399
Nicolas Azari <nicodinh@azari.fr>
2017-04-18 23:14:59 +0200

    Adding View

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,23 @@
 'use strict';
 
-import React from 'react';
+import React    from 'react';
 import { Text } from 'react-native';
+import { View } from 'react-native';
 
 const Header = () => {
-  const { textStyle } = styles;
-  return <Text style={textStyle} >Albums !</Text>;
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle} >Albums !</Text>
+    </View>
+  );
 };
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
