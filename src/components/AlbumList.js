1c1465720706fc578b50ec4bf08fb2a9e5867f9a
Nicolas Azari <nicodinh@azari.fr>
2017-04-20 08:31:44 +0200

    Added my first state :p

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,15 +3,21 @@ import { View, Text }       from 'react-native';
 import axios                from 'axios';
 
 class AlbumList extends Component {
+  state = {
+    albums: []
+  };
 
   componentWillMount() {
     console.log('Will mount');
-    //debugger;
+    //debugger; // <-- debugger is a break point to debugging the app, is not a comment
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-      .then(response => console.log(response));
+      .then(response => this.setState({
+        albums: response.data
+      }));
   }
 
   render() {
+    console.log(this.state)
     return (
       <View>
         <Text>Album List !</Text>
