3b2af7003c7bb62fd9aab9292aad0a459c1e15a6
Nicolas Azari <nicodinh@azari.fr>
2017-04-20 07:32:58 +0200

    Updated my dump component (presentation) to a container component (class), play wuth lifecycle method and debugger

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,20 @@
-import React          from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List !</Text>
-    </View>
-  );
+class AlbumList extends Component {
+
+  componentWillMount() {
+    console.log('Will mount');
+    //debugger;
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>Album List !</Text>
+      </View>
+    );    
+  }
 };
 
 export default AlbumList;
