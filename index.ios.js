3d60dcb96e13744bde7e9655bd3dd10aed50654a
Nicolas Azari <nicodinh@azari.fr>
2017-04-24 00:00:46 +0200

    making content scrollable

--- index.ios.js
+++ index.ios.js
@@ -20,7 +20,7 @@ import AlbumList  from './src/components/AlbumList';
 // same as
 
 const App = () => (
-  <View>
+  <View style={{ flex:1 }}>
     <Header headerText={'Albums'} />
     <AlbumList />
   </View> 
