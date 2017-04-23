944e2576879f64e2f5e8b2df6b41de7525b5a78d
Nicolas Azari <nicodinh@azari.fr>
2017-04-24 00:49:24 +0200

    Styling button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,11 +2,35 @@ import React    from 'react';
 import { Text, TouchableOpacity } from 'react-native'; 
 
 const Button = (props) => {
+  const { buttonStyle, textStyle } = styles;
   return (
-    <TouchableOpacity>
-      <Text>Click me bitch !!</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>
+        Click me bitch !!
+      </Text>
     </TouchableOpacity> 
   );
 };
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
+}
 
 export default Button;
