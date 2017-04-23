111bcf6f10d1eaa0f44c486421b940e01b9ca6f4
Nicolas Azari <nicodinh@azari.fr>
2017-04-24 01:17:03 +0200

    Better reusable component with props and children

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,12 @@
-import React    from 'react';
+import React                      from 'react';
 import { Text, TouchableOpacity } from 'react-native'; 
 
-const Button = ({ onPress }) => {
+const Button = ({ onPress, children }) => {
   const { buttonStyle, textStyle } = styles;
   return (
     <TouchableOpacity onPress={onPress} style={buttonStyle}>
       <Text style={textStyle}>
-        Click me bitch !!
+        {children}
       </Text>
     </TouchableOpacity> 
   );
