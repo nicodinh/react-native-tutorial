d5b50c113e73c22af4fca3d91121b99de8b28dc3
Nicolas Azari <nicodinh@azari.fr>
2017-04-23 14:01:06 +0200

    Added Image

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
-import React          from 'react';
-import { Text } from 'react-native';
+import React            from 'react';
+import { Text, Image }  from 'react-native';
 
 import Card from './Card';
 import CardSection from './CardSection';
@@ -8,8 +8,14 @@ const AlbumDetail = (props) => {
   return (
     <Card>
       <CardSection>
+        <Image style={{width: 50, height: 50}} source={{uri: props.album.image}} />
+      </CardSection>
+      <CardSection>
         <Text>{props.album.title}</Text>
       </CardSection>
+      <CardSection>
+        <Text>{props.album.artist}</Text>
+      </CardSection>      
     </Card>
   );
 };
