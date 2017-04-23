d591615d4f7b8b4617cedb431dc2d88b26a710eb
Nicolas Azari <nicodinh@azari.fr>
2017-04-23 08:44:53 +0200

    Render albums from an array with map and key index

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -9,18 +9,24 @@ class AlbumList extends Component {
 
   componentWillMount() {
     console.log('Will mount');
-    //debugger; // <-- debugger is a break point to debugging the app, is not a comment
+    //debugger; // <-- debugger is a break point to debugging the app, it is not a comment
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({
         albums: response.data
       }));
   }
 
+  renderAlbums() {
+    return this.state.albums.map((album, index) => 
+      <Text key={index}>{album.title}</Text>
+    );
+  }
+
   render() {
     console.log(this.state)
     return (
       <View>
-        <Text>Album List !</Text>
+        {this.renderAlbums()}
       </View>
     );    
   }
