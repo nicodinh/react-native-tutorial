07897df1aadb6799e79e98ddc5e4c11a85d7f57e
Nicolas Azari <nicodinh@azari.fr>
2017-04-23 23:46:46 +0200

    Better style, added image with automatic scaling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -5,18 +5,32 @@ import Card           from './Card';
 import CardSection    from './CardSection';
 
 const AlbumDetail = ({album}) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const { 
+    title,
+    artist,
+    thumbnail_image,
+    image
+  } = album;
+  const { 
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    headerTextStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image style={thumbnailStyle} source={{uri: thumbnail_image}} />          
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{uri: image}} />
       </CardSection>
     </Card>
   );
@@ -24,13 +38,26 @@ const AlbumDetail = ({album}) => {
 
 const styles= {
   headerContentStyle: {
-    paddingLeft: 4,
     justifyContent: 'space-around',
     flexDirection: 'column'
+  },
+  headerTextStyle: {
+    fontSize: 18
   },
   thumbnailStyle: {
     width: 50,
     height: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null    
   }
 }
 export default AlbumDetail;
