635d187d45a77b57f5a726bc91bb3d577d3c8ede
Nicolas Azari <nicodinh@azari.fr>
2017-05-08 17:01:27 +0200

    Initialize Firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 
 import { Header } from './components/common';
+import firebaseConfig from './config/firebase';
 
 class App extends Component {
+  componenentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+  }
 
   render() {
+    console.log(firebaseConfig);
     return (
       <View>
         <Header headerText='Authentication' />  
---
+++ auth/src/config/README.md
@@ -0,0 +1,12 @@
+Create firebase.json with firebase web config as json format
+
+```json
+{
+  "apiKey": "xxxx",
+  "authDomain": "xxx.firebaseapp.com",
+  "databaseURL": "https://xxx.firebaseio.com",
+  "projectId": "xxx",
+  "storageBucket": "xxx.appspot.com",
+  "messagingSenderId": "xxx"
+}
+```
