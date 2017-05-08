ee4ee7b34412e29c0a0343741ccf64dd4b97027d
Nicolas Azari <nicodinh@azari.fr>
2017-05-08 17:51:22 +0200

    Getting text from state

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import { View, TextInput }  from 'react-native';
+import { TextInput }  from 'react-native';
 
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+
+  // this.setState({ text }) === this.setState({ text: text })
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{ height:20, width: 100 }} />
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height:20, width: 100 }}
+          />
         </CardSection>
 
         <CardSection />
