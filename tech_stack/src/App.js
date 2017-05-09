3cfd40c7e24b24ed5c1843e6034ca6ec932d7252
Nicolas Azari <nicodinh@azari.fr>
2017-05-09 16:29:53 +0200

    Initialize Redux

---
+++ tech_stack/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View }             from 'react-native';
+import { Provider }         from 'react-redux';
+import { createStore }      from 'redux';
+
+import reducers from './reducers';
+
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
+      <View />
+    </Provider>
+  );
+};
+
+export default App;
