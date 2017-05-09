ef242913e2dff6e82fda52d42e09945572f3be68
Nicolas Azari <nicodinh@azari.fr>
2017-05-09 11:09:47 +0200

    Improving login form

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -2,23 +2,51 @@ import React, { Component } from 'react';
 import { Text }             from 'react-native';
 import firebase             from 'firebase';
 
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '', error: '' };
+  state = { email: '', password: '', error: '', loading: false };
 
   onButtonPress() {
     const { email, password } = this.state;
 
-    this.setState({ error: '' });
+    this.setState({ error: '', loading: true });
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .catch(() => {
-            this.setState({ error: 'Authentication Failed.' });
-          });
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
+
+  onLoginFail() {
+    this.setState({
+      loading: false,
+      error: 'Authentication Failed.'
+    });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '' // overkill
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size='small' />
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
   }
 
   // this.setState({ text }) === this.setState({ text: text })
@@ -27,7 +55,7 @@ class LoginForm extends Component {
       <Card>
         <CardSection>
           <Input
-            placeholder='your@email.com'
+            placeholder='your@email .com'
             label='Email'
             value={this.state.email}
             onChangeText={email => this.setState({ email })}
@@ -49,9 +77,7 @@ class LoginForm extends Component {
         </Text>
 
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log in
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
