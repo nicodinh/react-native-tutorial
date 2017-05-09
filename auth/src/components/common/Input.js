cec6170fc83e4822b80a446d9b04580cb857e817
Nicolas Azari <nicodinh@azari.fr>
2017-05-09 10:42:29 +0200

    Creating an Activity Spinner

--- auth/src/components/common/Input.js
+++ auth/src/components/common/Input.js
@@ -1,5 +1,5 @@
-import React, { Component }      from 'react';
-import { TextInput, View, Text } from 'react-native';
+import React, { Component }       from 'react';
+import { TextInput, View, Text }  from 'react-native';
 
 const Input = ({ 
   label,
