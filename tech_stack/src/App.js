ac1eca8eaac580ac7616d75c4fe9a73916b32367
Nicolas Azari <nicodinh@azari.fr>
2017-05-09 16:41:53 +0200

    Reusing existing components from previous project to render the Header

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,12 +3,15 @@ import { View }             from 'react-native';
 import { Provider }         from 'react-redux';
 import { createStore }      from 'redux';
 
-import reducers from './reducers';
+import reducers   from './reducers';
+import { Header } from './components/common';
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
-      <View />
+      <View>
+        <Header headerText='Tech Stack' />
+      </View>
     </Provider>
   );
 };
