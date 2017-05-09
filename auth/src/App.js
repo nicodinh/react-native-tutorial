bbeaf10ab0da6047db16ce40ce397b62d40d9063
Nicolas Azari <nicodinh@azari.fr>
2017-05-09 11:22:00 +0200

    Handling authentication events

--- auth/src/App.js
+++ auth/src/App.js
@@ -7,8 +7,18 @@ import { Header }     from './components/common';
 import LoginForm      from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp(firebaseConfig);
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
   }
 
   render() {
