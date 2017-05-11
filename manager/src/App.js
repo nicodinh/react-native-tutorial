0b7ef5c1d765c24c29c772cf6c77d751867c2ae7
Nicolas Azari <nicodinh@azari.fr>
2017-05-11 16:46:14 +0200

    Setup Firebase authentication

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import { Text, View }       from 'react-native';
 import { Provider }         from 'react-redux';
 import { createStore }      from 'redux';
+import firebase             from 'firebase';
 
-import reducers from './reducers';
+import firebaseConfig from './config/firebase.json';
+import reducers       from './reducers';
 
 class App extends Component {
+  // state = { loggedIn: null };
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
+
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.setState({ loggedIn: true });
+    //   } else {
+    //     this.setState({ loggedIn: false });
+    //   }
+    // });
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
