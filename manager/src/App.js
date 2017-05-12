8450c1a013da2a22c791858c3795b38896fe44ba
Nicolas Azari <nicodinh@azari.fr>
2017-05-12 16:04:33 +0200

    Added ReduxThunk to make asynchronous Action Creator

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View }       from 'react-native';
 import { Provider }         from 'react-redux';
-import { createStore }      from 'redux';
+import {
+  createStore,
+  applyMiddleware
+}                           from 'redux';
 import firebase             from 'firebase';
+import ReduxThunk           from 'redux-thunk'; // it's a middleware for a Redux application
 
 import firebaseConfig from './config/firebase.json';
 import reducers       from './reducers';
@@ -14,8 +17,10 @@ class App extends Component {
   }
 
   render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,8 +1,10 @@
+import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED
 } from './types';
 
+// Action Creator `emailChanged`
 export const emailChanged = (text) => {
   return {
     type: EMAIL_CHANGED,
@@ -10,9 +12,18 @@ export const emailChanged = (text) => {
   };
 };
 
+// Action Creator `passwordChanged`
 export const passwordChanged = (text) => {
   return {
     type: PASSWORD_CHANGED,
     payload: text
+  };
+};
+
+// Action Creator `loginUser` using ReduxThunk
+export const loginUser = ({ email, password }) => {
+  return (dispatch) => {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(user => console.log(user));
   };
 };
