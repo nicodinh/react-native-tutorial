d3cd38968390b69abd1bf68a2fd8534ffd876211
Nicolas Azari <nicodinh@azari.fr>
2017-05-15 11:10:56 +0200

    Improving creating account

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from './types';
 
 // Action Creator `emailChanged`
@@ -28,9 +29,16 @@ export const loginUser = ({ email, password }) => {
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-          .then(user => loginUserSuccess(dispatch, user));
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
       });
   };
+};
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL  
+  });
 };
 
 // dispatch will call when firebase request is complete 
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,7 +1,8 @@
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from '../actions/types';
 import { displayStates } from '../lib/displayStates';
  
@@ -17,13 +18,15 @@ export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
       displayStates(state, action, 'email');
-      return { ...state, email: action.payload }
+      return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
       displayStates(state, action, 'password');
-      return { ...state, password: action.payload }
+      return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
       displayStates(state, action, 'user');
-      return { ...state, user: action.payload }
+      return { ...state, user: action.payload };
+    // case LOGIN_USER_FAIL:
+    //   return { ...state };      
     default:
       return state;
   }
