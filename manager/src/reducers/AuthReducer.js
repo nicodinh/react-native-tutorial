e6d442c98830016554abce19c3be18c37a7938c8
Nicolas Azari <nicodinh@azari.fr>
2017-05-15 14:42:08 +0200

    Debugging auth reducer

--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -16,17 +16,50 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMAIL_CHANGED:
-      displayStates(state, action, 'email');
+      //displayStates(state, action, 'email');
+      emailStates(state, action);
       return { ...state, email: action.payload };
     case PASSWORD_CHANGED:
-      displayStates(state, action, 'password');
+      //displayStates(state, action, 'password');
+      passwordStates(state, action);
       return { ...state, password: action.payload };
     case LOGIN_USER_SUCCESS:
-      displayStates(state, action, 'user');
-      return { ...state, user: action.payload };
+      //displayStates(state, action, 'user');
+      loginSuccessStates(state, action);
+      return { ...state, user: action.payload, error: '' };
     case LOGIN_USER_FAIL:
+      //displayStates(state, action, 'error');
+      loginFailStates(state, action);
       return { ...state, error: 'Authentication Failed.', password: '' };
     default:
       return state;
   }
+};
+
+const emailStates = (state, action) => {
+  console.log('Action Type:\n\t', action.type);
+  console.log('Previous States:\n\t', state);
+  console.log('Current States:\n\t', { ...state, 'email': action.payload });
+  console.log('');
+};
+
+const passwordStates = (state, action) => {
+  console.log('Action Type:\n\t', action.type);
+  console.log('Previous States:\n\t', state);
+  console.log('Current States:\n\t', { ...state, 'password': action.payload });
+  console.log('');
+};
+
+const loginSuccessStates = (state, action) => {
+  console.log('Action Type:\n\t', action.type);
+  console.log('Previous States:\n\t', state);
+  console.log('Current States:\n\t', { ...state, 'user': action.payload, error: '' });
+  console.log('');
+};
+
+const loginFailStates = (state, action) => {
+  console.log('Action Type:\n\t', action.type);
+  console.log('Previous States:\n\t', state);
+  console.log('Current States:\n\t', { ...state, error: 'Authentication Failed.', password: '' });
+  console.log('');
 };
