a8d5ccc6bcec97dae31ba0d2b39ff01c3f4ac4a7
Nicolas Azari <nicodinh@azari.fr>
2017-05-16 16:44:43 +0200

    navigating between routes

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,4 +1,5 @@
-import firebase from 'firebase';
+import firebase     from 'firebase';
+import { Actions }  from 'react-native-router-flux';
 
 import {
   EMAIL_CHANGED,
@@ -54,4 +55,6 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+
+  Actions.employeeList();
 };
