c754867ab9e18b07e8d5ea1213994cde4b768008
Nicolas Azari <nicodinh@azari.fr>
2017-05-17 11:47:05 +0200

    Handling form updates at the reducer level

--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -4,12 +4,16 @@ import {
 import { logStates } from '../lib/logStates';
 
 const INITIAL_STATE = {
-
+  name: '',
+  phone: '',
+  shift: ''
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-
+    case EMPLOYEE_UPDATE:
+      // action.payload === { props: 'name', value: 'jane' }
+      return { ...state, [action.payload.name]: action.payload.value };
     default:
       return state;
   }
