ed15937a3c33f3f35efc01afc12574d88800dedc
Nicolas Azari <nicodinh@azari.fr>
2017-05-19 16:38:16 +0200

    Fetching data from Firebase

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -3,7 +3,8 @@ import { Actions }  from 'react-native-router-flux';
 
 import {
   EMPLOYEE_UPDATE,
-  EMPLOYEE_CREATE
+  EMPLOYEE_CREATE,
+  EMPLOYEES_FETCH_SUCCESS
 } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -24,6 +25,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
         dispatch({ type: EMPLOYEE_CREATE});
 
         Actions.employeeList({ type: 'reset' });
+      });
+  };
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
       });
   };
 };
