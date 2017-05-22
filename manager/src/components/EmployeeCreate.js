c37e5790ed579f4a638241a0d36c3d24d580979a
Nicolas Azari <nicodinh@azari.fr>
2017-05-22 12:34:50 +0200

    Reusing the employee form

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -21,6 +21,8 @@ class EmployeeCreate extends Component {
   }
 
   render() {
+    console.log(this.props.employee);
+    
     return (
       <Card>
         <CardSection>
--- manager/src/components/ListItem.js
+++ manager/src/components/ListItem.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import { Text }             from 'react-native';
+import React,  { Component } from 'react';
+import { 
+  Text,
+  TouchableWithoutFeedback,
+  View
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
 
 import { CardSection } from './common';
 
 class ListItem extends Component {
+  onRowPress() {
+    Actions.employeeCreate({ employee: this.props.employee });
+  }
+
   render() {
     const { name } = this.props.employee;
 
     return (
-      <CardSection>
-        <Text styl={styles.titleStyle}>
-          {name}
-        </Text>
-      </CardSection>  
+      <TouchableWithoutFeedback
+        onPress={this.onRowPress.bind(this)}
+      >
+        <View>
+          <CardSection>
+            <Text styl={styles.titleStyle}>
+              {name}
+            </Text>
+          </CardSection>
+        </View>  
+      </TouchableWithoutFeedback>
     );
   }
 }
