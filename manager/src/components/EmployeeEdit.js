b72f75327ffa4f104ad83f7d011cb65c305dfebd
Nicolas Azari <nicodinh@azari.fr>
2017-05-23 12:16:10 +0200

    Updating Firebase records

--- manager/src/components/EmployeeEdit.js
+++ manager/src/components/EmployeeEdit.js
@@ -8,7 +8,10 @@ import {
   CardSection,
   Button
 } from './common';
-import { employeeUpdate } from '../actions';
+import {
+  employeeUpdate,
+  employeeSave
+} from '../actions';
 
 class EmployeeEdit extends Component {
   componentWillMount() {
@@ -19,7 +22,9 @@ class EmployeeEdit extends Component {
 
   onButtonPress() {
     const { name, phone, shift } = this.props;
-    console.log(name, phone, shift);
+    
+    //console.log(name, phone, shift);
+    this.props.employeeSave({ name, phone, shift, uid: this.props.employee.uid });
   }
 
   render() {
@@ -42,4 +47,7 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeEdit);
+export default connect(mapStateToProps, {
+  employeeUpdate,
+  employeeSave
+})(EmployeeEdit);
