3df485f44ed0c9abfb563e00c1c5c1d0aa78ace9
Nicolas Azari <nicodinh@azari.fr>
2017-05-26 11:03:59 +0200

    Modals as a reusable component

---
+++ manager/src/components/common/Confirm.js
@@ -0,0 +1,12 @@
+import React                  from 'react';
+import { Text, View, Modal }  from 'react-native';
+
+//import { CardSection, Button } from './'; <-- we can do that due to cycling dependancies
+import { CardSection }  from './CardSection';
+import { Button }       from './Button';
+
+const Confirm = () => {
+
+};
+
+export { Confirm };
