b41e030d05309cd1318211c49acfc9bfc47cdfd0
Nicolas Azari <nicodinh@azari.fr>
2017-05-29 11:10:21 +0200

    Creating a new Ball component and display it on the screen

--- swipe/main.js
+++ swipe/main.js
@@ -1,12 +1,18 @@
-import Expo from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Expo   from 'expo';
+import React  from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Ball from './src/Ball';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <Ball />
       </View>
     );
   }
