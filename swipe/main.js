01ec5d8316047d4f78a885b7cf0b08ecb0d4e3c6
Nicolas Azari <nicodinh@azari.fr>
2017-05-29 11:27:19 +0200

    Animating a ball

--- swipe/main.js
+++ swipe/main.js
@@ -21,9 +21,7 @@ class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
 
