8a3da78850bad69e0bb1be4fe4db9238f65fb2e9
Nicolas Azari <nicodinh@azari.fr>
2017-05-30 15:36:00 +0200

    Interplay between components

--- swipe/main.js
+++ swipe/main.js
@@ -6,13 +6,35 @@ import {
   View
 } from 'react-native';
 
-import Ball from './src/Ball';
+//import Ball from './src/Ball';
+import Deck from './src/Deck';
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
+  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
+  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
+  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' }
+];
 
 class App extends React.Component {
+  renderCard(item) {
+    return (
+      <Text>{item.Text}</Text>
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Ball />
+        {/*<Ball />*/}
+        <Deck
+          data={DATA}
+          renderCard={this.renderCard}
+        />
       </View>
     );
   }
